03cc0eb3bae746a6e788999f40c0ec4b23738554
yan358941877 <yan358941877@163.com>
2017-04-18 16:54:39 +0800

    歌词的实现

--- app/MusicPlayer.js
+++ app/MusicPlayer.js
@@ -32,13 +32,17 @@ function MusicPlayer(playerNode) {
     this.process_bar = root.find('.music-panel .right-part .operate .process');
     // 进度条
     this.process_now_bar = root.find('.music-panel .right-part .operate .process .now');
+    // 歌词控件
+    this.lyric_ct = root.find('.music-lyric');
     //console.log(this.lastTime.text()+'aaa');
     this.music = new Audio();
     this.volume = 0.5;
     // 音乐风格相关
     this.channel = "public_fengge_liuxing";
     this.channelIndex = 1;
-    
+    // 音乐歌词
+    this.lyrics = [];
+
     this.playFlag = false;
     this.clickFlag = false;
     this.init();
@@ -81,6 +85,10 @@ MusicPlayer.prototype = {
         //点击进度条
         this.clickProcessBar = this.clickProcessBar.bind(this);
         this.process_bar.on('click', this.clickProcessBar);
+
+        //歌词的实现
+        this.getLyric = this.getLyric.bind(this);
+
     },
     // 播放或暂停歌曲
     clickPlay: function(event){
@@ -95,6 +103,7 @@ MusicPlayer.prototype = {
             this.btn_play.addClass("icon-play")
             this.playFlag = !this.playFlag;
         }
+        console.log(this.lyrics);
     },
     // 下一首歌曲
     clickNextMusic: function(event){
@@ -127,9 +136,9 @@ MusicPlayer.prototype = {
             let minute = Math.floor(this.music.duration/60);
             let second = Math.floor(this.music.duration%60);
             if(second<10){
-                this.lastTime.text("-0" + minute + ':0' + second);
+                this.lastTime.text("0" + minute + ':0' + second);
             }else{
-                this.lastTime.text("-0" + minute + ':' + second);
+                this.lastTime.text("0" + minute + ':' + second);
             }
             
         }
@@ -138,16 +147,23 @@ MusicPlayer.prototype = {
 
         this.music.ontimeupdate = function(){
             let time_percent = 100*context.music.currentTime/time + '%';
-            let minute = Math.floor((time-context.music.currentTime)/60);
-            let second = Math.floor((time-context.music.currentTime)%60);
-            if(second<10){
-                context.lastTime.text("-0" + minute + ':0' + second);
+            let minute = Math.floor(context.music.currentTime/60);
+            let second = Math.floor(context.music.currentTime)%60;
+            let rest_minute = Math.floor((time-context.music.currentTime)/60);
+            let test_second = Math.floor((time-context.music.currentTime)%60);
+            if(test_second<10){
+                context.lastTime.text("-0" + rest_minute + ':0' + test_second);
             }else{
-                context.lastTime.text("-0" + minute + ':' + second);
+                context.lastTime.text("-0" + rest_minute + ':' + test_second);
             }
             // 修改进度条
             context.process_now_bar.css('width', time_percent);
             // 添加歌词功能
+            let lyric = context.lyrics[minute][second];
+            if(lyric){
+                context.lyric_ct.text(lyric)
+            }
+            
         }
         
     },
@@ -232,6 +248,9 @@ MusicPlayer.prototype = {
             url: url
         }).done(function(data){
             console.log(JSON.parse(data).song[0]);
+            // 获取歌词
+            context.getLyric(JSON.parse(data).song[0].sid);
+            
             context.music.src = JSON.parse(data).song[0].url;
             context.picture.eq(0).attr("src",JSON.parse(data).song[0].picture);
             // 更换音乐名称
@@ -239,7 +258,7 @@ MusicPlayer.prototype = {
             // 更换歌手名称
             context.singer.eq(0).text(JSON.parse(data).song[0].artist);
             // 更换音乐剩余时间
-
+            
             // 不应该在这里从audio对象中获取音乐的时间，因为音乐可能还没有加载出来
             //console.log(context.music.currentTime);
             //context.lastTime.text('-' + context.music.currentTime);
@@ -251,6 +270,40 @@ MusicPlayer.prototype = {
             context.playFlag = true;
             context.btn_play.removeClass('icon-play');
             context.btn_play.addClass('icon-pause');
+        })
+    },
+    // 获取歌词，并且根据歌词对应的时间存放到lyric数组中对应的位置
+    getLyric(id){
+        let url = "http://jirenguapi.applinzi.com/fm/getLyric.php?&sid="+ id;
+        let context = this;
+        $.ajax({
+            method: 'get',
+            url: url
+        }).done(function(data){
+            let lyrics = JSON.parse(data).lyric;
+            //console.log(lyrics);
+            let lyricArray  = lyrics.split('\n');
+            //console.log(lyricArray);
+            for(let i = 1; i< lyricArray.length;i++){
+                // 一句歌词的形式：[00:32.56]轻轻叠上了微微雨
+                let a_lyric = lyricArray[i].split(']');
+                // 得到 ["[00:32.56", "轻轻叠上了微微雨"]
+                // 如果歌词的第一部分并不是时间
+                if(a_lyric.length < 2 || isNaN(a_lyric[0][1])){
+                    continue;
+                }
+                // a_lyric[0] --> [00:32.56
+                // a_lyric[0].substr(1) --> 00:32.56
+                // a_lyric[1] --> "轻轻叠上了微微雨"
+                let time = a_lyric[0].substr(1).split(':');
+                let minute = parseInt(time[0]);
+                let second = parseInt(time[1]);
+                if(!context.lyrics[minute]){
+                    context.lyrics[minute] = [];
+                }
+                context.lyrics[minute][second] = a_lyric[1];
+            }
+            //console.log(context.lyrics);
         })
     }
    
